7de98f825ad2890911e62cce9e73b4de63b1d95e
Kauet Lima <kauet123@live.com>
2022-12-11 17:09:14 -0300

    Primeiro commit

---
+++ 1016/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ConsumoAutomovel = 12, TempoGasto, VelocidadeMedia;
+            double ConsumoFinal;
+
+            TempoGasto = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            VelocidadeMedia = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            ConsumoFinal = TempoGasto * VelocidadeMedia;
+            ConsumoFinal = ConsumoFinal / ConsumoAutomovel;
+
+            Console.WriteLine(ConsumoFinal.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
